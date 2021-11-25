5c0f014b19e6dd17ba841a74a5f7962d9432ef92
Alessandro Sosso <alesosso@gmail.com>
2021-11-25 11:50:10 +0100

    File creation

---
+++ modules/var.py
@@ -0,0 +1,8 @@
+import os
+
+# Directories
+DATA_DIR = os.path.join(os.getcwd(), 'data')
+CREDENTIALS_DIR = os.path.join(os.getcwd(), 'credentials')
+
+# Files
+TELEGRAM_BOT_CREDENTIALS_FILE = os.path.join(CREDENTIALS_DIR, 'ghislieri_bot_credentials.gbcr')
